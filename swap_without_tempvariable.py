f61397c1fb5146b0085f54b78f9f901fd3a6b22e
AL-AmeenS <78216644+AL-AmeenS@users.noreply.github.com>
2021-03-06 08:57:40 +0530

    Add files via upload

---
+++ swap_without_tempvariable.py
@@ -0,0 +1,19 @@
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+
+a,b = b,a
+
+print('The value of a after swapping: ',  a)
+print('The value of b after swapping: ',  b)
+
+
+
+a = int(input("enter the value of a:"))
+b  = int(input("enter the value of b:"))
+
+a=a+b
+b=a-b
+a=a-b
+
+print('The value of a after swapping: ',  a)
+print('The value of b after swapping: ',  b)
